fd7488f1454f8073ce458495d4cc8d01108245a7
phyoko <pkkoedu777@gmail.com>
2023-03-11 15:58:32 +0630

    Installed material-icon and modified some text

--- components/about.jsx
+++ components/about.jsx
@@ -1,4 +1,8 @@
 import {Typography} from "@mui/material"
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 const About = () => {
     return (
@@ -7,12 +11,57 @@ const About = () => {
             <Typography variant="h3" component="h1">
                 Material UI
             </Typography>
-            <Typography variant="p">
+            <Typography variant="p" color={"yellow"}>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Delectus voluptatem facere dolores at eaque eligendi quidem
                 asperiores quis, amet a voluptatum eum iusto sit hic. Dolor aut
                 cum facilis sit.
             </Typography>
+            <hr />
+
+            <div>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>Accordion 1</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography>Accordion 2</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Suspendisse malesuada lacus ex, sit amet
+                            blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion disabled>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel3a-content"
+                        id="panel3a-header"
+                    >
+                        <Typography>Disabled Accordion</Typography>
+                    </AccordionSummary>
+                </Accordion>
+            </div>
         </>
     )
 }
