3318489c032c5f2a93cdabd0f48bc2f07d6765a0
phyoko <pkkoedu777@gmail.com>
2023-03-11 15:41:05 +0630

    Installed material-ui and react-router

---
+++ components/about.jsx
@@ -0,0 +1,20 @@
+import {Typography} from "@mui/material"
+
+const About = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <Typography variant="h3" component="h1">
+                Material UI
+            </Typography>
+            <Typography variant="p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Delectus voluptatem facere dolores at eaque eligendi quidem
+                asperiores quis, amet a voluptatum eum iusto sit hic. Dolor aut
+                cum facilis sit.
+            </Typography>
+        </>
+    )
+}
+
+export default About
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import About from "../components/about"
+import App from "./App"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        {/* <App /> */}
+        <About />
+    </React.StrictMode>
+)
